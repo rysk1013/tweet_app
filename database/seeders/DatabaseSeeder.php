7c2485982f46d83e87ad218327974b5eb47b5058
rysk1013 <rysk2908@gmail.com>
2022-07-23 17:21:04 +0900

    add sample data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([TweetsSeeder::class]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
