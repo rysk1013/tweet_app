d58828c2220d12ce21fabda8b508d25d04303818
rysk1013 <rysk2908@gmail.com>
2022-07-23 19:48:34 +0900

    add laravel-breeze

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,9 @@ Route::put('/tweet/update/{tweetId}', App\Http\Controllers\Tweet\Update\PutContr
     ->name('tweet.update.put')->where('tweetId', '[0-9]+');
 Route::delete('/tweet/delete/{tweetId}', App\Http\Controllers\Tweet\DeleteController::class)
     ->name('tweet.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
