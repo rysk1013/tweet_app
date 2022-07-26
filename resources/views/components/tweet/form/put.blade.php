ea50c974dba404aa3e690b89755dcd12a1cb04f1
rysk1013 <rysk2908@gmail.com>
2022-07-26 23:29:03 +0900

    fix textarea

--- resources/views/components/tweet/form/put.blade.php
+++ resources/views/components/tweet/form/put.blade.php
@@ -7,9 +7,7 @@
         @method('PUT')
         @csrf
         <div class="mt-1">
-            <textarea name="tweet" row="3" class="focus:ring-blue-400 focus:border-blue-400 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2" placeholder="つぶやきを入力">
-                {{ $tweet->content }}
-            </textarea>
+            <textarea name="tweet" row="3" class="focus:ring-blue-400 focus:border-blue-400 mt-1 block w-full sm:text-sm border-gray-300 rounded-md p-2" placeholder="つぶやきを入力">{{ $tweet->content }}</textarea>
         </div>
         <p class="mt-2 text-sm text-gray-500">
             140文字まで
