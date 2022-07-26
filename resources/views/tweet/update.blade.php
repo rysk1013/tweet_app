f059f8696b0acf560aff2a92c37eafb3108fcf51
rysk1013 <rysk2908@gmail.com>
2022-07-26 23:04:51 +0900

    add update layout

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>つぶやきアプリ</title>
-</head>
-<body>
-    <a href="{{ route('tweet.index') }}">< 戻る</a>
-    <p>投稿フォーム</p>
-    <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post" novalidate>
-        @method('PUT')
-        @csrf
-        <label for="tweet-content">つぶやき</label>
-        <span>140文字まで</span>
-        <textarea name="tweet" id="tweet-content">{{ $tweet->content }}</textarea>
-        @error('tweet')
-            <p style="color: red;">{{ $message }}</p>
-        @enderror
-        <p><input type="submit" value="編集"></p>
-    </form>
-</body>
-</html>
+<x-layout title="編集 | つぶやきアプリ">
+    <x-layout.single>
+        <h2 class="text-center text-blue-500 text-4xl font-blod mt-8 mb-8">
+            つぶやきアプリ
+        </h2>
+        <x-tweet.form.put :tweet="$tweet"></x-tweet.form.put>
+    </x-layout.single>
+</x-layout>
