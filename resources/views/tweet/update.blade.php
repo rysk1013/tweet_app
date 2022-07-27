5cc14efaee6daa6ceb66aab8a7dcd993aeb0d6c3
rysk1013 <rysk2908@gmail.com>
2022-07-27 21:52:10 +0900

    add breadcrumbs

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -3,6 +3,13 @@
         <h2 class="text-center text-blue-500 text-4xl font-blod mt-8 mb-8">
             つぶやきアプリ
         </h2>
+        @php
+            $breadcrumbs = [
+                ['href' => route('tweet.index'), 'label' => 'TOP'],
+                ['href' => '#', 'label' => '編集'],
+            ]
+        @endphp
+        <x-element.breadcrumbs :breadcrumbs="$breadcrumbs"></x-element.breadcrumbs>
         <x-tweet.form.put :tweet="$tweet"></x-tweet.form.put>
     </x-layout.single>
 </x-layout>
