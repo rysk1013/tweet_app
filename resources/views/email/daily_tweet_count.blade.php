608b90d0f9bf1543216b9445b680afea903b46b7
rysk1013 <rysk2908@gmail.com>
2022-08-27 23:38:36 +0900

    add daily tweet count mail

---
+++ resources/views/email/daily_tweet_count.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# 昨日は{{ $count }}件のつぶやきが追加されました！
+
+{{ $toUser->name }}さん、こんにちは！
+
+昨日は{{ $count }}件のつぶやきが追加されましたよ！最新のつぶやきを見に行きましょう。
+
+@component('mail::button')
+  つぶやきを見に行く
+@endcomponent
+
+@endcomponent
